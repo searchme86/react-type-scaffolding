fac8b89dd7674e07be029d8adde02ca7fb2a311a
searchme86 <searchme86@gmail.com>
2022-02-10 19:51:05 +0900

    🔥 redirected done

--- src/Server/useAuth.ts
+++ src/Server/useAuth.ts
@@ -1,17 +1,20 @@
-import { getAuth, onAuthStateChanged, User } from "@firebase/auth";
-import { app } from "../Server/FirebaseConfig";
-import { useEffect, useRef, useState } from "react";
+import { getAuth, onAuthStateChanged, User } from '@firebase/auth';
+import { app } from '../Server/FirebaseConfig';
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const useAuth = () => {
+  const navigate = useNavigate();
   const auth = getAuth(app);
   const [user, setUser] = useState<User | null>(null);
   let mounted = useRef<boolean>(false);
   useEffect(() => {
     mounted.current = true;
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      console.log(user);
+      console.log('여기 user는 무엇', user);
       if (user) {
         if (mounted.current) setUser(user);
+        navigate('/home');
       } else {
         if (mounted.current) setUser(null);
       }
