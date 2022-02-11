2e9d8992e7ba7f827f19c09d0689bb459379e45f
searchme86 <searchme86@gmail.com>
2022-02-12 00:41:04 +0900

    🔥  common style

--- src/Views/Components/Input.style.ts
+++ src/Views/Components/Input.style.ts
@@ -1,12 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { lInputWidth } from '../../Core/Interface/lStyle';
 
-export const MainInputWrapper = styled.div`
-  width: 681px;
-`;
-
-export const SearchInput = styled.input`
+const CommonInput = styled.input`
   display: block;
   width: 100%;
   box-sizing: border-box;
-  padding: 5px 0 5px 12px;
+  padding: 5px 5px 5px 12px;
 `;
+
+export const InputWrapper = styled.div<lInputWidth>`
+  width: ${({ inputWithBtn }: lInputWidth) => inputWithBtn}px;
+`;
+
+export const SearchInput = styled(CommonInput).attrs({
+  placeholder: '검색어를 입력하세요',
+})``;
+
+export const VideoInput = styled(CommonInput).attrs({
+  placeholder: 'url을 입력해주세요',
+})``;
+
+export const TagInput = styled(CommonInput).attrs({
+  placeholder: '태그를 입력하세요',
+});
