a7abc568b01ae3c4faa5283d204f61c3eef2cf77
searchme86 <searchme86@gmail.com>
2022-02-17 14:38:11 +0900

    ⚡️ interface modified

--- src/Core/Config/AssetPath.ts
+++ src/Core/Config/AssetPath.ts
@@ -4,6 +4,7 @@ import logo from '../../Views/Assets/Images/logo.png';
 import RegisterBtn from '../../Views/Assets/Images/Register.png';
 import AlertBtn from '../../Views/Assets/Images/Alert.png';
 import TagDelete from '../../Views/Assets/Images/TagDelete.png';
+import LoginImg from '../../Views/Assets/Images/Welcome.png';
 
 export const LogoImg: lAsset = {
   name: 'logo',
@@ -34,5 +35,13 @@ export const TagRemove: lAsset = {
   info: {
     src: `${TagDelete}`,
     alt: '태그버튼 지우기',
+  },
+};
+
+export const LogIn: lAsset = {
+  name: 'LogIn',
+  info: {
+    src: `${LoginImg}`,
+    alt: '로그인 이미지',
   },
 };
--- src/Core/Interface/lStyle.ts
+++ src/Core/Interface/lStyle.ts
@@ -7,4 +7,5 @@ export interface lBoxStyle {
   fromRight?: number;
   bgColor?: string;
   textColor?: string;
+  ImgWidth?: number;
 }
