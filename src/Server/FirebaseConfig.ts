e3b011924e789b42057b3e6a052bb67d0e9ca50a
searchme86 <searchme86@gmail.com>
2022-02-17 14:38:46 +0900

    ⚡️ server files modified

--- src/Server/FirebaseConfig.ts
+++ src/Server/FirebaseConfig.ts
@@ -1,20 +1,24 @@
-// import { FirebaseApp, getApps, initializeApp } from "firebase/app";
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 
 interface lFire {
   [key: string]: string | undefined;
 }
 
 const firebaseConfig: lFire = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyAZ6GJ-uGkZW1Dm4XCBju1L9XNTGV9WvtQ',
+  authDomain: 'reactmusic-8976e.firebaseapp.com',
+  projectId: 'reactmusic-8976e',
+  storageBucket: 'reactmusic-8976e.appspot.com',
+  messagingSenderId: '563524552606',
+  appId: '1:563524552606:web:460b4b8285e1d11304f88f',
 };
 
 const app = initializeApp(firebaseConfig);
 
-// export default app;
 export { app };
---
+++ src/Server/UseAuth.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, User } from '@firebase/auth';
+import { app } from './FirebaseConfig';
+import { useNavigate } from 'react-router-dom';
+
+interface UserCxt {
+  user: User;
+  setUser: (user: User) => void;
+}
+
+export const UserContext = React.createContext<UserCxt | null>(null);
+export const defaultHeaders: any = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+};
+
+const hours = new Date().getHours();
+const muninutes = new Date().getMinutes();
+
+const UseAuth = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+  const auth = getAuth(app);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+      console.log('firebaseUser', firebaseUser);
+      if (firebaseUser) {
+        try {
+          const token = await firebaseUser.getIdToken();
+          defaultHeaders.Authorization = `Bearer ${token}`;
+
+          const res = await fetch('/users/me', {
+            method: 'GET',
+            headers: defaultHeaders,
+          });
+
+          console.log('users/me 서버를 거쳐 나온 res의 값은?', res);
+
+          if (res.status === 200) {
+            console.log('서버 결과 200 입니다');
+            const user = await res.json();
+            setUser(user);
+            navigate('/');
+            console.log('실제유저가 등록되어 홈 으로 이동합니다');
+          }
+
+          if (res.status === 404) {
+            console.log('서버결과 404 입니다, 유저를 등록해야 합니다');
+            const res = await fetch('/users', {
+              method: 'POST',
+              headers: defaultHeaders,
+            });
+            const user = await res.json();
+            setUser(user);
+            console.log('실제유저가 등록되어 홈 으로 이동합니다');
+          }
+
+          console.log(`현재시각은 ${hours}:${muninutes}, 현재token은`, token);
+          console.log('defaultHeaders', defaultHeaders);
+          setUser(firebaseUser);
+        } catch (error) {
+          console.log('At UseAuth.tsx, Error is found', error);
+        }
+      } else {
+        delete defaultHeaders.Authorizations;
+        setUser(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [auth, navigate]);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UseAuth;
