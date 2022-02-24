208b7b0bdb28c2ab7f669b012a9cc2363c72cd73
searchme86 <searchme86@gmail.com>
2022-02-24 14:40:17 +0900

    ⚡️ redux setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import GlobalStyle from "./Views/Assets/Styles/GlobalStyle";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import GlobalStyle from './Views/Assets/Styles/GlobalStyle';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducrer from './Store/Modules/index';
+const store = createStore(rootReducrer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
+    <GlobalStyle />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
