1d229acbb749fc0c8b9619f347f5b385d237c0e8
searchme86 <searchme86@gmail.com>
2022-03-01 10:12:38 +0900

    ⚡️ tag componentet

--- src/Views/Components/TagFunc/TagNew.tsx
+++ src/Views/Components/TagFunc/TagNew.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { lTagN } from '../../../Core/Interface/lForm';
 import { TagCompItem, TagCompList } from './Tag.style';
-import { v4 as uuidv4 } from 'uuid';
 
 function TagNew({ onDeleteTag, value }: lTagN) {
   let tag = (
     <TagCompList>
-      <TagCompItem onClick={() => onDeleteTag(value)} key={uuidv4()}>
-        {value}
-      </TagCompItem>
+      <TagCompItem onClick={() => onDeleteTag(value)}>{value}</TagCompItem>
     </TagCompList>
   );
   return <>{tag}</>;
